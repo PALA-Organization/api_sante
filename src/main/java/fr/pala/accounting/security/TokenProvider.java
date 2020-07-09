b4c076aa1634168fa5c8d99dcde648e9d8689d27
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-07-09 21:45:20 +0200

    Remove unused vars and clean

--- src/main/java/fr/pala/accounting/security/TokenProvider.java
+++ src/main/java/fr/pala/accounting/security/TokenProvider.java
@@ -1,6 +1,9 @@
 package fr.pala.accounting.security;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,7 +16,6 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Date;
 import java.util.stream.Collectors;
 
 @Slf4j
